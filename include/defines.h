1f5095fb086a80453472fa48e6afa3b94f785f0c
Daan van Driel <dtvandriel@gmail.com>
2018-12-15 17:02:43 +0100

    added esp32 touch pin readout

---
+++ include/defines.h
@@ -0,0 +1,17 @@
+#define TOUCH_CENTER    14
+#define TOUCH_RING1     27
+#define TOUCH_RING2     33
+#define TOUCH_RING3     32
+/*
+   Touch Sensor Pin Layout
+   T0 = GPIO4
+   T1 = GPIO0
+   T2 = GPIO2
+   T3 = GPIO15
+   T4 = GPIO13
+   T5 = GPIO12
+   T6 = GPIO14
+   T7 = GPIO27
+   T8 = GPIO33
+   T9 = GPIO32
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,37 @@
 #include <Arduino.h>
 #include <Milight.h>
+#include "../include/defines.h"
 
 Milight Milight;
+
+int buff(int);
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
 
-    Milight.begin("UPC9486202", "LNRYWFSH");
+    // Milight.begin("UPC9486202", "LNRYWFSH");
 
-    // Interval between commands
-    Milight.setCommandInterval(20);
+    // // Interval between commands
+    // Milight.setCommandInterval(20);
 
-    // The extremeness of the brightness curve
-    Milight.setBrightnessCurve(2);
+    // // The extremeness of the brightness curve
+    // Milight.setBrightnessCurve(2);
 }
 
 void loop() {
-    // Milight.brightness(0, 0);
-    // delay(1000);
-    // Milight.brightness(255, 0);
-    // delay(1000);
-    //Milight.on(group);
-    // Milight.off(1);
-    // delay(1000);
-    // Milight.on(1);
-    // delay(1000);
-    //Milight.hue(value, group);
-    //Milight.white(group); 
-    //Milight.saturation(value, group);
+    Serial.print(buff(TOUCH_CENTER));
+    Serial.print("\t");
+    Serial.print(buff(TOUCH_RING1));
+    Serial.print("\t");
+    Serial.print(buff(TOUCH_RING2));
+    Serial.print("\t");
+    Serial.println(buff(TOUCH_RING3));
+    // Milight.run();
+}
 
-    Milight.run();
+int buff(int pin) {
+  int out = (50 - touchRead(pin));
+  if(out > 0) return (out + 2);
+  else return 0;
 }
